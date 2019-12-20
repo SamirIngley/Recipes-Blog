04bb65bb18aac7aac0d089a0277ca773e893f488
SamirIngley <samir.ingle7@gmail.com>
2019-12-20 00:26:09 -0800

    tests added

--- makerecipe/settings.py
+++ makerecipe/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
+SECRET_KEY = 'hello'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
